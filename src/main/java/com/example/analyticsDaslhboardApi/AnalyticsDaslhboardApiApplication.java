518a2c729d96c7253693d81540a484f07048c42a
AarushGaikwad <Aarushgaikwad789@gmail.com>
2025-07-12 16:21:58 +0530

    Initial commit

---
+++ src/main/java/com/example/analyticsDaslhboardApi/AnalyticsDaslhboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.analyticsDaslhboardApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsDaslhboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsDaslhboardApiApplication.class, args);
+	}
+
+}
