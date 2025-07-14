1ec8e7b6fc818551059aa3273d4c6a8278c086b3
AarushGaikwad <Aarushgaikwad789@gmail.com>
2025-07-14 12:13:26 +0530

    updated the controller class

--- src/main/java/com/example/analyticsDaslhboardApi/controller/AnalyticsController.java
+++ src/main/java/com/example/analyticsDaslhboardApi/controller/AnalyticsController.java
@@ -1,9 +1,15 @@
 package com.example.analyticsDaslhboardApi.controller;
 
+import com.example.analyticsDaslhboardApi.dto.RegionOrderVolumeDto;
+import com.example.analyticsDaslhboardApi.dto.RegistrationStatsDto;
+import com.example.analyticsDaslhboardApi.dto.TopProductDto;
 import com.example.analyticsDaslhboardApi.service.AnalyticsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/analytics")
@@ -12,19 +18,23 @@ public class AnalyticsController {
 
     private final AnalyticsService analyticsService;
 
-    public void getDailyRegistrations(){
-
+    @GetMapping("/users/daily")
+    public List<RegistrationStatsDto> getDailyRegistrations(){
+        return analyticsService.getDailyUserRegistrations();
     }
 
-    public void getWeeklyRegistrations(){
-
+    @GetMapping("/users/weekly")
+    public List<RegistrationStatsDto> getWeeklyRegistrations(){
+        return analyticsService.getWeeklyUserRegistrations();
     }
 
-    public void getOrderVolumeByRegion(){
-
+    @GetMapping("/orders/region")
+    public List<RegionOrderVolumeDto> getOrderVolumeByRegion(){
+        return analyticsService.getOrderVolumeByRegion();
     }
 
-    public void getTopProducts(){
-        
+    @GetMapping("/top-product")
+    public List<TopProductDto> getTopProducts(){
+        return analyticsService.getTop5OrderedProduct();
     }
 }
